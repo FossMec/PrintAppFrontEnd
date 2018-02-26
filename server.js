e07d500574a660f6b7b56a088a0bbead5d234767
123vivekr <123vivekr@gmail.com>
2018-02-26 21:40:41 +0530

    fixed pdf preview

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var path = require('path');
+var fs = require("fs");
+var bodyParser = require('body-parser');
+var multer  = require('multer');
 
 var app = express();
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(multer({ dest: '/tmp/'}));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname,'landing.html'));
@@ -14,9 +19,21 @@ app.get('/public/css/home_style.css', function (req, res) {
 app.get('/public/assets/test1.jpeg', function (req, res) {
   res.sendFile(path.join(__dirname,'public', 'assets', 'test1.jpeg'));
 });
+app.get('/public/assets/grass.jpeg', function (req, res) {
+  res.sendFile(path.join(__dirname,'public', 'assets', 'grass.jpeg'));
+});
+app.get('/public/assets/pdf1.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname,'public', 'assets', 'pdf1.jpg'));
+});
 app.get('/login.html', function (req, res) {
   res.sendFile(path.join(__dirname,'login.html'));
 });
+app.get('/public/js/pdf.worker.js', function (req, res) {
+  res.sendFile(path.join(__dirname,'public', 'js', 'pdf.worker.js'));
+});
+app.get('/public/js/pdf.js', function (req, res) {
+  res.sendFile(path.join(__dirname,'public', 'js', 'pdf.js'));
+}); 
 app.get('/public/css/style.css', function (req, res) {
   res.sendFile(path.join(__dirname,'public', 'css', 'style.css'));
 });
@@ -31,6 +48,24 @@ app.get('/user.html', function (req, res) {
 });
 app.get('/public/css/user_style.css', function (req, res) {
   res.sendFile(path.join(__dirname,'public', 'css', 'user_style.css'));
+});
+app.post('/file_upload', function (req, res) {
+   var file = __dirname + "/" + req.files.file.name;
+   
+   fs.readFile( req.files.file.path, function (err, data) {
+      fs.writeFile(file, data, function (err) {
+         if( err ){
+            console.log( err );
+            }else{
+               response = {
+                  message:'File uploaded successfully',
+                  filename:req.files.file.name
+               };
+            }
+         console.log( response );
+         res.end( JSON.stringify( response ) );
+      });
+   });
 });
 app.listen(3000);
 
